ecd7267e8058941fa018f6ef0ca6285e64319ebb
Cleiton Anderson Profilio dos Santos <cleitonandersonprofilio@hotmail.com>
2019-05-26 18:30:23 -0400

    Teste

--- src/java/br/com/backEnd/CamadaSimulacao.java
+++ src/java/br/com/backEnd/CamadaSimulacao.java
@@ -2,6 +2,7 @@ package br.com.backEnd;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 public class CamadaSimulacao{
     static Random gerador = new Random();
@@ -16,6 +17,7 @@ public class CamadaSimulacao{
         Math.exp(x);
         tn = t + (RTT/2) + x;
         listaTempo.add(tn);
+        Collections.sort(listaTempo);
     }
 }
 
